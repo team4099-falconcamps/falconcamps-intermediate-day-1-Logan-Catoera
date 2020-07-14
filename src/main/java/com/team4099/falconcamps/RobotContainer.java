35e14052c98874c795004e497d8fa663a29375bb
Logan-Catoera <catj8548@chsphantoms.org>
2020-07-14 17:28:51 +0000

    RobotContainer

--- src/main/java/com/team4099/falconcamps/RobotContainer.java
+++ src/main/java/com/team4099/falconcamps/RobotContainer.java
@@ -1,7 +1,11 @@
 package com.team4099.falconcamps;
 
 public class RobotContainer {
+    private final IntakeSubsystem subsystem = new IntakeSubsystem();
+    XboxController driverController = new XboxController(0);
+   
     public RobotContainer() {
+        new Trigger(driverController::getAButton).whileActive(new MyCommand(subsystem));
 
     }
 }
