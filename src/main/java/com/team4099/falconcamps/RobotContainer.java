481c0e82ae4b442423526d7be1e6ea83a1edecae
Logan-Catoera <catj8548@chsphantoms.org>
2020-07-14 19:03:31 +0000

    robotcontainer

--- src/main/java/com/team4099/falconcamps/RobotContainer.java
+++ src/main/java/com/team4099/falconcamps/RobotContainer.java
@@ -1,18 +1,20 @@
 package com.team4099.falconcamps;
 
 import com.team4099.falconcamps.commands.shooter.ShootSparkPIDCommand;
+import com.team4099.falconcamps.commands.shooter.ShooterSparkPIDIdleCommand;
+import com.team4099.falconcamps.subsystems.ShooterPIDSubsystem;
 import com.team4099.falconcamps.subsystems.ShooterSparkPID;
 
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 
 public class RobotContainer {
-    private final ShooterSparkPID subsystem = new ShooterSparkPID();
+    private final ShooterSparkPID sparkSubsystem = new ShooterSparkPID();
     private final ShooterPIDSubsystem subsystem = new ShooterPIDSubsystem();
     XboxController driverController = new XboxController(0);
 
     public RobotContainer() {
-        new Trigger(driverController::getAButton).whileActiveOnce(new ShootSparkPIDCommand(subsystem));
-
+        new Trigger(driverController::getAButton).whileActiveOnce(new ShootSparkPIDCommand(sparkSubsystem));
+        sparkSubsystem.setDefaultCommand(new ShooterSparkPIDIdleCommand(sparkSubsystem));
     }
 }
